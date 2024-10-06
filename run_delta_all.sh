179b231353a8ab3c3e1ec2ebdb1a82683da8d82e
TongZhang-ML <tozhang@dt-login02.delta.ncsa.illinois.edu>
2024-10-05 23:08:51 -0500

    edited

--- run_delta_all.sh
+++ run_delta_all.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 #
 
-./run_delta_slurm.sh run_gsm_split0.sh
+./run_delta_slurm.sh -s run_gsm_split0.sh
 
-./run_delta_slurm.sh run_gsm_split1.sh
+./run_delta_slurm.sh -s run_gsm_split1.sh
 
-./run_delta_slurm.sh run_math_split0.sh
+./run_delta_slurm.sh -s run_math_split0.sh
 
-./run_delta_slurm.sh run_math_split1.sh
+./run_delta_slurm.sh -s run_math_split1.sh
 
 
 
