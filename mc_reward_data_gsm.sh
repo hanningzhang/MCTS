c48157d73a59207510f115816280c2640b7fa40e
rpan <research4pan@gmail.com>
2024-10-06 19:31:18 -0500

    Change output directory to a larger disk

--- mc_reward_data_gsm.sh
+++ mc_reward_data_gsm.sh
@@ -15,7 +15,7 @@ touch ${mark}.ongoing
 python mc_reward_data_gsm.py \
     --completion_model_name_or_path deepseek-ai/deepseek-math-7b-rl \
     --dataset_path HanningZhang/gsm-deepseek \
-    --output_dir smooth_reward_data \
+    --output_dir /scratch/bckr/rpan2/smooth_reward_data \
     --tensor_parallel_size 1 \
     --num_gpus 4 \
     --local_rank $1 \
